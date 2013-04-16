38dcfd5e01629bac8acecda2756737a00d22f6e8
Denis Yagofarov <denyago@gmail.com>
2013-04-16 23:56:53 +0300

    Fix issue with 500 error on Song update

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
   attr_accessible :artist, :file, :name
+
+  def as_json(options={})
+    opts = options.deep_merge({:except =>[:created_at, :updated_at]})
+    super(opts)
+  end
 end
